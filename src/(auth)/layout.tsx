3cb2b2153001df5d0e2a4bfedbff82eca4db23fa
Sumeet_Gond <sumitv9009@gmail.com>
2025-08-10 13:56:53 +0530

    added api for user reputation

--- src/(auth)/layout.tsx
+++ src/(auth)/layout.tsx
@@ -1,7 +1,27 @@
-import React from "react"
+"use client"
+import { useAuthStore } from "@/store/Auth"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 
-function layout() {
-  return <div>layout</div>
+const layout = ({ childern }: { childern: React.ReactNode }) => {
+  //to Check whether there is session or not
+  const { session } = useAuthStore()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session) {
+      router.push("/")
+    }
+  }, [session, router])
+  if (session) {
+    return null
+  }
+
+  return (
+    <div className="">
+      <div className="">{childern}</div>
+    </div>
+  )
 }
 
 export default layout
